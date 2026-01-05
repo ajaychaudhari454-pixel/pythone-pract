6f33603b36ea3a4f739ac4a2871f4db6975433c6
ajaychaudhari454-pixel <ajaychaudhari454@gmail.com>
2026-01-05 06:48:56 +0530

    simple python print program

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("hello")
+
+sales=25000
+name="ajay"
+rating=4.5
+
+if sales>25000:
+    print("ok")
+elif sales>20000:
+    print("a")
+else:
+    print("no")
+    
+print(type(sales))
+print(type(name))
+print(type(rating))
