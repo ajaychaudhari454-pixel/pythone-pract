067d86eb8b6cb5f624652367a7d9b8b9bde26ec9
ajaychaudhari454-pixel <ajaychaudhari454@gmail.com>
2026-01-21 07:09:17 +0530

    Add files via upload

---
+++ practice set.py
@@ -0,0 +1,12 @@
+# This program compares two numbers
+# and checks whether the first number is greater than the second.
+
+# Taking first number input from the user and converting it to integer
+a = int(input("Enter a number: "))
+
+# Taking second number input from the user and converting it to integer
+b = int(input("Enter another number: "))
+
+# Comparing both numbers using greater than (>) operator
+# This will return True if a is greater than b, otherwise False
+print("a is greater than b:", a > b)
