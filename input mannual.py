3615b323bca03d37739ce20a2d50452a51e84d01
ajaychaudhari454-pixel <ajaychaudhari454@gmail.com>
2026-01-21 07:08:00 +0530

    Add files via upload

---
+++ input mannual.py
@@ -0,0 +1,8 @@
+# This program takes input from the user
+# and prints the data type of the input.
+
+# Taking input from the user
+a = input("Enter anything: ")
+
+# Printing the type of the input variable
+print(type(a))
