c904a5caf3ae8aa057c7b303632aab5bc09445ba
ajaychaudhari454-pixel <ajaychaudhari454@gmail.com>
2026-01-21 07:06:48 +0530

    Add files via upload

---
+++ avarage.py
@@ -0,0 +1,14 @@
+# This program calculates the average of two numbers
+# entered by the user.
+
+# Taking first number input from the user
+a = int(input("Enter first number: "))
+
+# Taking second number input from the user
+b = int(input("Enter second number: "))
+
+# Calculating the average
+average = (a + b) / 2
+
+# Printing the result
+print("The average of two numbers is:", average)
