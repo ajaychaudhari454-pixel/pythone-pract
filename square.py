f219252f40194388355be21fa68ec16eef129b76
ajaychaudhari454-pixel <ajaychaudhari454@gmail.com>
2026-01-21 07:13:06 +0530

    Add files via upload

---
+++ square.py
@@ -0,0 +1,8 @@
+# This program calculates the square of a number
+# entered by the user.
+
+# Taking a number as input from the user
+a = int(input("Enter a number: "))
+
+# Calculating and printing the square of the number
+print("The square of the number is:", a ** 2)
