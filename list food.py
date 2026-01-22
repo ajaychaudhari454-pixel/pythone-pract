d4ae1b26daf8dfd3e91246437c1a678b734bd021
ajaychaudhari454-pixel <ajaychaudhari454@gmail.com>
2026-01-22 21:30:02 +0530

    Add files via upload

---
+++ list food.py
@@ -0,0 +1,17 @@
+# Empty list banayi fruits store karne ke liye
+fruit = []
+
+# User se pehla fruit ka naam lena
+f1 = input("Enter the name of fruit 1: ")
+fruit.append(f1)   # List me first fruit add kiya
+
+# User se dusra fruit ka naam lena
+f2 = input("Enter the name of fruit 2: ")
+fruit.append(f2)   # List me second fruit add kiya
+
+# User se teesra fruit ka naam lena
+f3 = input("Enter the name of fruit 3: ")
+fruit.append(f3)   # List me third fruit add kiya
+
+# Final list ko print karna
+print("The fruits you entered are:", fruit)
