e5aa6e08b3ad624e5776a4bde02f4f7ee2df14a7
ajaychaudhari454-pixel <ajaychaudhari454@gmail.com>
2026-02-02 07:26:52 +0530

    Add files via upload
    added user name and language dictionary input program

---
+++ language.py
@@ -0,0 +1,19 @@
+s={}
+
+d=input("enter you name:-")
+e=input("enter your language:-")
+
+s.update({d:e})
+
+d=input("enter you name:-")
+e=input("enter your language:-")
+
+s.update({d:e})
+
+d=input("enter you name:-")
+e=input("enter your language:-")
+
+s.update({d:e})
+
+
+print(s)
